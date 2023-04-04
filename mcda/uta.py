fix: use weigths actual values in linear function params calculation

mcda/uta.py
@@ -86,7 +86,9 @@ class LinearUTASolver:
         if self.problem.status != 1:
             raise RuntimeError("Solver must be solved before accessing the results")
         linear_coeff = (
-            self.alternative_values
+            np.array(
+                [self.weights[k].varValue for k in range(self.alternatives.shape[1])]
+            )
             / (self.alternatives.max(axis=0) - self.alternatives.min(axis=0))
         ) * (self.is_gain - 1 * (1 - self.is_gain))
         standalone_coeff = self.alternatives.min(