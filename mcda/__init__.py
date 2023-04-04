a43bd8e5ac360e9f7cd3269ec8deb5395c303457
Bartosz Stachowiak <sbartekt@op.pl>
2023-04-05 00:05:37 +0200

    chore: allow for flat import of LinearUTASolver

--- mcda/__init__.py
+++ mcda/__init__.py
@@ -1,5 +1,6 @@
 from . import order
 from .electreetrib import ElectreTriBSolver
 from .promethee import PrometheeSolver
+from .uta import LinearUTASolver
 from .srf import calc_srf
 from .types import FeatureSpec
