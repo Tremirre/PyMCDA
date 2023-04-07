2e9c2b51cc3dc5d71183d9698618193fda246410
Bartosz Stachowiak <sbartekt@op.pl>
2023-04-07 14:07:58 +0200

    chore: remove unnecessary setup.py setting

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     name="pymcda",
     version="0.1.0",
     description="Python library for Multi-Criteria Decision Analysis",
-    package_dir={"": "pymcda"},
     packages=find_packages("pymcda"),
     package_data={"pymcda": ["py.typed"]},
     author="Bartosz Stachowiak",
